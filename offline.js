aafdc2f32c30a28d0bba5154c80775cc109b5811
ubg42 <redtomatotiger@gmail.com>
2026-03-01 12:41:06 +1100

    Add Files

---
+++ offline.js
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+<!--  Start AdMetricsPro Head Code for MathPlayground.com - Games Layout -->
+<script src="https://qd.admetricspro.com/js/mathplayground/games-layout-loader.js"></script>
+<!--  End AdMetricsPro Head Code for MathPlayground.com - Games Layout -->
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-F2L1YLQS2E"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-F2L1YLQS2E');
+</script>
+
+
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <!-- Basic Page Needs
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>Math Games | Math Playground | Fun for Kids</title>
+
+
+    <meta name="keywords" content="math games, math, Math Playground, problem solving, math practice, 1st grade math, 2nd grade math, 3rd grade math, 4th grade math, 5th grade math, 6th grade math, fraction games, money games, algebra games, geometry games, multiplication games, addition games, subtraction games, division games, math facts, free math games, online math games, common core math">
+
+    <meta name="description" content="Free, online math games and more at MathPlayground.com! Problem solving, logic games and number puzzles kids love to play.">
+
+    <meta name="robots" content="noindex">
+
+
+    <!-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="stylesheet" href="/css/main-v2.css">
+	<link rel="stylesheet" href="/css/navigation-v2.css">
+
+	<style>
+
+	h6 {
+    	font-size: 1.8rem;
+    	line-height: 1.6;
+   	 	letter-spacing: 0;
+		color: royalblue;
+	}
+		
+	h2 {
+			color: royalblue;
+		}
+		
+	h3 {
+			color: royalblue;
+		}
+		
+	h4 {
+			color: royalblue;
+		}
+	
+    </style>
+	
+
+
+
+    <!-- Favicon
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="icon" type="image/png" href="favicon.ico">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="167x167" href="/apple-touch-icon-167x167.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+
+
+    
+
+
+                
+	
+
+    
+	
+</head>
+<body>
+
+
+
+<div id="siteHeader">
+    
+  <a href="https://www.mathplayground.com"><img src="/images/logo-mp.png" alt="Math Playground" class="logo-header-new"></a>
+                                               
+  <div id="revCatchButtonContainer">
+    <a href="#" style="display:none" id="theLoginButton"><button class=" subscription-button" onclick="window.dispatchEvent(window.catchrclogin); return false;">Sign In</button></a>
+	<button id="theSubscribeButton" class="subscription-button" style="display:none" type="button" onclick="window.location.assign('/subscribe');">Subscribe</button>
+    <a href="#" style="display:none" id="theLogoutButton"><button class="subscription-button" onclick="handleLogoutClick()">Logout</button></a>
+    <a href="#" style="display:none" id="theProfileButton"><button class="revcatch-profile subscription-button" onclick="return false;">Profile</button></a>
+  </div>
+</div>
+	
+	
+
+	
+
+
+
+
+<!-- Primary Page Layout
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+ <!-- End Container-->
+	
+
+
+
+<div id="header-nav" class="container">
+  <a href="/math-games.html" class="nav-btn nav-math">Math Games</a>
+  <a href="/wordproblems.html" class="nav-btn nav-stories">Math Stories</a>
+  <a href="/logic-games.html" class="nav-btn nav-logic">Logic Lab</a>
+  <div id="funbutton"><a href="/brain-boosts.html" class="nav-btn nav-breaks">Brain Boosts</a></div>
+  <a href="/game_directory.html" class="nav-btn nav-library">Library</a>
+</div>		
+	<div class="container">
+
+<header class="grade-level-navigation">
+	  <a href="/kindergarten_games.html" class="grade-btn grade-k">K</a>
+  <a href="/grade_1_games.html" class="grade-btn grade-1">1</a>
+  <a href="/grade_2_games.html" class="grade-btn grade-2">2</a>
+  <a href="/grade_3_games.html" class="grade-btn grade-3">3</a>
+  <a href="/grade_4_games.html" class="grade-btn grade-4">4</a>
+  <a href="/grade_5_games.html" class="grade-btn grade-5">5</a>
+  <a href="/grade_6_games.html" class="grade-btn grade-6">6+</a>
+	</header>
+
+	
+<div class="col-xs-12">
+	<div style="padding-top:140px; padding-bottom:140px" class="eight columns offset-by-two">
+		<h2><b>404: Page Not Found</b></h2>
+		<h3>We're sorry this page is missing.<br>It may have been renamed or removed.<br>Please try our <a href="/game_directory.html">Game Library</a> for a full list of activities.</h3>
+	</div>
+</div>	
+
+<div>
+   <img src="/css_images/math-playground-links.png" class="centerImageLinks" alt="Math Playground">
+</div>
+
+<nav aria-label="Footer navigation" class="footer-links">
+  <div class="linkrow">
+    <div class="linkcolumn">
+      <h4>MATH PLAYGROUND</h4>
+      <ul>
+        <li><a href="/kindergarten_games.html">Kindergarten Math</a></li>
+        <li><a href="/grade_1_games.html">1st Grade Math</a></li>
+        <li><a href="/grade_2_games.html">2nd Grade Math</a></li>
+        <li><a href="/grade_3_games.html">3rd Grade Math</a></li>
+        <li><a href="/grade_4_games.html">4th Grade Math</a></li>
+        <li><a href="/grade_5_games.html">5th Grade Math</a></li>
+        <li><a href="/grade_6_games.html">6th Grade Math</a></li>
+        <li><a href="/thinkingblocks.html">Word Problems</a></li>
+      </ul>
+    </div>
+
+    <div class="linkcolumn">
+      <h4>MATH PRACTICE</h4>
+      <ul>
+        <li><a href="/index_addition_subtraction.html">Addition Practice</a></li>
+        <li><a href="/index_addition_subtraction.html">Subtraction Practice</a></li>
+        <li><a href="/index_multiplication_division.html">Multiplication Practice</a></li>
+        <li><a href="/index_multiplication_division.html">Division Practice</a></li>
+        <li><a href="/index_fractions.html">Fraction Practice</a></li>
+        <li><a href="/index_prealgebra.html">Ratio Practice</a></li>
+        <li><a href="/index_prealgebra.html">Prealgebra Practice</a></li>
+        <li><a href="/index_geometry.html">Geometry Practice</a></li>
+      </ul>
+    </div>
+
+    <div class="linkcolumn">
+      <h4>LEARNING LAB</h4>
+      <ul>
+        <li><a href="/logic-games.html">Logic Challenges</a></li>
+        <li><a href="/index_classic.html">Classics Collection</a></li>
+        <li><a href="/index_brainy.html">Math Puzzles</a></li>
+        <li><a href="/index_find.html">Spatial Reasoning</a></li>
+        <li><a href="/index_coder.html">Coding Activities</a></li>
+        <li><a href="/index_strategy.html">Strategy Challenges</a></li>
+        <li><a href="/ASB_Index.html">Two-Player Activities</a></li>
+        <li><a href="/learning-games.html">Learning Activities</a></li>
+      </ul>
+    </div>
+
+    <div class="linkcolumn">
+      <h4>PREMIUM MATH</h4>
+      <ul>
+        <li><a href="/thinkagons.html">Thinkagons</a></li>
+        <li><a href="/reflectagons.html">Reflectagons</a></li>
+        <li><a href="/bonus_games.html#mathgames">Bonus Math Practice</a></li>
+        <li><a href="/bonus_games.html#problemsolving">Bonus Problem Solving</a></li>
+        <li><a href="/bonus_games.html#wordgames">Bonus Word Practice</a></li>
+        <li><a href="/math-playground-focused-classroom-plan.html">Focused Classroom</a></li>
+        <li><a href="/math-playground-focused-family-plan.html">Focused Family</a></li>
+        <li><a href="/subscribe.html">Subscribe</a></li>
+      </ul>
+    </div>
+  </div>
+  
+  <div class="linkrow">
+    <div class="linkcolumn">
+      <h4>FRACTION FOREST</h4>
+      <ul>
+        <li><a href="/fraction_forest_part1.html">Unit Fractions</a></li>
+        <li><a href="/fraction_forest_part2.html">Unit Fractions Pro</a></li>
+        <li><a href="/fraction_forest_playground.html">Fraction Playground 1</a></li>
+        <li><a href="/fraction_forest_part3.html">Equal Fractions</a></li>
+        <li><a href="/fraction_forest_part4.html">Equal Fractions Pro</a></li>
+        <li><a href="/fraction_forest_playground2.html">Fraction Playground 2</a></li>
+        <li><a href="/fraction_forest_part5.html">Adding Fractions</a></li>
+        <li><a href="/fraction_forest_part6.html">Adding Fractions Pro</a></li>
+        <li><a href="/fraction_forest_playground3.html">Fraction Playground 3</a></li>
+      </ul>
+    </div>
+
+    <div class="linkcolumn">
+      <h4>THINKING BLOCKS</h4>
+      <ul>
+        <li><a href="/tb_addition_jr/index.html">TB Junior</a></li>
+        <li><a href="/tb_addition/index.html">TB Addition</a></li>
+        <li><a href="/tb_multiplication/index.html">TB Multiplication</a></li>
+        <li><a href="/tb_fractions/index.html">TB Fractions</a></li>
+        <li><a href="/tb_ratios/index.html">TB Ratios</a></li>
+        <li><a href="/thinking_blocks_modeling_tool/index.html">Modeling Tool</a></li>
+        <li><a href="/printable-thinking-blocks.html">Printables</a></li>
+        <li><a href="/thinkingblocks.html">Videos</a></li>
+        <li><a href="/wpdatabase/wpindex.html">Word Problems</a></li>
+      </ul>
+    </div>
+
+    <div class="linkcolumn">
+      <h4>NUMBER PUZZLES</h4>
+      <ul>
+        <li><a href="/sum_stacks.html">Sum Stacks</a></li>
+        <li><a href="/logic_number_sequence.html">Number Sequence</a></li>
+        <li><a href="/logic_sum_links_2.html">Sum Links</a></li>
+        <li><a href="/logic_sum_blocks.html">Sum Blocks</a></li>
+        <li><a href="/chain_sums.html">Chain Sums</a></li>
+		<li><a href="/premium_sum_slider.html">Sum Slider</a></li>
+        <li><a href="/stretch_sums.html">Stretch Sums</a></li>
+        <li><a href="/swap_sums.html">Swap Sums</a></li>
+        <li><a href="/logic_overlap_sums.html">Overlap Sums</a></li>
+      </ul>
+    </div>
+
+    <div class="linkcolumn">
+      <h4>TEACHING MATH</h4>
+      <ul>
+		<li><a href="/teachers_guides.html">Teachers' Guides</a></li>  
+        <li><a href="/blog-sweet-algebra-puzzles.html">Algebra Puzzles</a></li>
+        <li><a href="/blog-strategic-multiplication.html">Strategic Multiplication</a></li>
+        <li><a href="/blog-thinking-deeply-with-fractions.html">Fraction Tasks</a></li>
+        <li><a href="/blog-problem-solving-with-logic-games.html">Problem Solving</a></li>
+        <li><a href="/blog-teaching-3rd-grade-math.html">3rd Grade Math</a></li>
+        <li><a href="/blog-visual-math-tools.html">Visual Math Tools</a></li>
+        <li><a href="/blog-drift-boss.html">Drift Boss Math</a></li>
+        <li><a href="/blog-model-your-word-problems.html">Model Word Problems</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+<div>
+	<img src="/css_images/line-separator.png" class="centerImageLinks3" alt="footer">
+</div>
+
+
+
+
+<div class="site-footer-links">
+  <div class="site-footer-seal">
+    <a href="https://www.kidsafeseal.com/certifiedproducts/mathplayground.html">
+      <img src="https://www.kidsafeseal.com/sealimage/1142216312560877248/mathplayground_medium_darktm.png" class="site-footer-coppa" alt="Kid Safe Seal">
+    </a>
+  </div>
+
+  <div class="site-footer-nav">
+    <a href="/terms_of_service.html" class="site-footer-link">Terms of Service</a>
+    <a href="/about.html" class="site-footer-link">About Us</a>
+    <a href="/support.html" class="site-footer-link">Get Support</a>
+    <a href="/accessibility/conformance-report.html" class="site-footer-link">Accessibility Report</a>
+    <a href="/privacy_policy.html" class="site-footer-link site-footer-link-big">Privacy Policy</a>
+  </div>
+</div>
+<div class="site-footer-copyright">
+  Math Playground<sup>®</sup>, Thinking Blocks<sup>®</sup>, and Puzzle Playground<sup>®</sup> are registered trademarks of Math Playground LLC.<br>
+  Copyright © 2002–2026 Math Playground LLC • All Rights Reserved
+</div>
+
+</div> <!-- End Container-->
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<script id='HyDgbd_1s' src='https://prebidads.revcatch.com/ads.js' type='text/javascript' async></script> <script data-cfasync="false" type="text/javascript">(function(w,d,b,s,i){var cts=d.createElement(s);cts.async=true;cts.id='catchscript'; cts.dataset.appid=i;cts.src='https://tools.mathplayground.com/catch_rp.js'; document.head.appendChild(cts); }) (window,document,'head','script','rc-VDeAGw');</script>
+
+<script async src="/revCatchButtons.js"></script>
+<script src="libraries/lazysizes.min.js" async></script>
+</body>
+</html>
